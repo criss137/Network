d6b75e1bd8214d76bfb31305d68ee42dced155ae
Daniel Baud <criss137@live.fr>
2017-05-05 13:26:53 +0200

    #

--- main.c
+++ main.c
@@ -7,7 +7,18 @@
 
 int main(void){
     struct neuralNet *R = loadNetwork("network");
-    double input[8] = {1, 0.274825, 0.045000, 1.005000,1 ,0.348775, 0.039000, 1.100000};
+    
+    double first_hand = 1;
+    double first_age = 35;
+    double first_rank = 287;
+    double first_points = 172;
+    
+    double second_hand = 1;
+    double second_age = 26;
+    double second_rank = 95;
+    double second_points = 558;
+    
+    double input[8] = {first_hand, first_age/100, 1/first_rank, first_points/10000, second_hand, second_age/100, 1/second_rank, second_points/10000};
     double *res = malloc(2*sizeof(double));
     res = result(R, input);
     printf("JOUEUR1: %lf   JOUEUR2: %lf\n", res[0], res[1]);
@@ -15,4 +26,4 @@ int main(void){
     return 0;
 }
 
-/* ---*/
+/* --- */
