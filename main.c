a1f6f24733ffb59959453502f045c60edbbf0c83
Choqs <vincent-974@hotmail.com>
2017-05-06 02:32:22 +0200

    fully trained

--- main.c
+++ main.c
@@ -7,17 +7,17 @@
 
 int main(void){
     struct neuralNet *R = loadNetwork("network");
-    
+
     double first_hand = 1;
-    double first_age = 35;
-    double first_rank = 287;
-    double first_points = 172;
-    
+    double first_age = 30;
+    double first_rank = 1;
+    double first_points = 14500;
+
     double second_hand = 1;
-    double second_age = 26;
-    double second_rank = 95;
-    double second_points = 558;
-    
+    double second_age = 20;
+    double second_rank = 250;
+    double second_points = 250 ;
+
     double input[8] = {first_hand, first_age/100, 1/first_rank, first_points/10000, second_hand, second_age/100, 1/second_rank, second_points/10000};
     double *res = malloc(2*sizeof(double));
     res = result(R, input);
